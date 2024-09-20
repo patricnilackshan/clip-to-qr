8213f6bfaabe05029e3698c4c6e168b728e81090
Patric Nilackshan <126662734+patricnilackshan@users.noreply.github.com>
2024-09-20 13:53:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/home/patricnilackshan/miniconda3/bin/python
 import qrcode
 import tkinter as tk
 import pyperclip as pc
@@ -6,10 +7,10 @@ def main():
     info = pc.paste()
     window = tk.Tk()
 
-    qr_code_filename = "qr_code.png"
+    qr_code_filename = "clip2qr.png"
     qr_code = qrcode.make(info)
     qr_code.save(qr_code_filename)
-    window.title("QR Code")
+    window.title("Clip2QR")
     image = tk.PhotoImage(file=qr_code_filename)
     image_label = tk.Label(window, image=image)
     image_label.pack()
